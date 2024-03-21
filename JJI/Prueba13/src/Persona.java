7d989f8a86f9a41f8444bf25b598d9d5b50317ba
isabelmvi <isabel.murariu@gmail.com>
2024-03-21 11:01:12 +0100

    sobras

---
+++ JJI/Prueba13/src/Persona.java
@@ -0,0 +1,19 @@
+
+public class Persona {
+	protected int edad;
+
+	public Persona(int edad) {
+		super();
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
---
+++ JJI/Prueba13/src/PersonaApp.java
@@ -0,0 +1,11 @@
+
+public class PersonaApp {
+
+	public static void main(String[] args) {
+
+		Persona isabel = new Persona(18);
+		
+		
+	}
+
+}
